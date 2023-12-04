531459db95d1939b5228e928266f4e05958812a4
anshunathrnc <143875046+anshunathrnc@users.noreply.github.com>
2023-12-04 18:07:36 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+provider "azurerm" {
+    features {
+      
+    }
+  
+}
+# Create  a Resource Group
+resource "azurerm_resource_group" "anshu_rg" {
+    name = "anshuterraformrg"
+    location = "eastus"
+  
+}
+# Create a Storage Account
+resource "azurerm_storage_account" "anshusa" {
+  name                     = "terrafor_anshu_storage_account"
+  resource_group_name      = azurerm_resource_group.anshu_rg.name
+  location                 = azurerm_resource_group.anshu_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+  static_website {
+    index_document = "index.html"
+  }
+} 
+# Add a index.html file to storage Account
+resource "azurerm_storage_blob" "anshu_blob" {
+    name = "index.html"
+    storage_account_name   = azurerm_storage_account.anshusa.name
+  storage_container_name = "$web"
+  type = "Block"
+  content_type = "text/html"
+  source_content = "<h1> hellow alion, This is Naresh IT</h1>"
+  
+}
